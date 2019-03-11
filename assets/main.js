123abddceb0091abe86bc07678304368315baf65
Arrathilar <Wervvolf@gmail.com>
2019-03-12 00:56:59 +0200

    add webix js framework

--- assets/main.js
+++ assets/main.js
@@ -13,3 +13,17 @@
 //     }
 //   }
   
+
+"use strict";
+webix.ui({
+rows:[
+  { view:"template", 
+    type:"header", template:"My App!" },
+  { view:"datatable", 
+    autoConfig:true, 
+    data:{
+      title:"My Fair Lady", year:1964, votes:533848, rating:8.9, rank:5
+    }
+  }
+]
+});
