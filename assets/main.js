d3ea7f1eda4fb2782baf4101723912be9638ca4e
Arrathilar <Wervvolf@gmail.com>
2019-03-12 02:03:36 +0200

    add logo and some fix

--- assets/main.js
+++ assets/main.js
@@ -14,16 +14,16 @@
 //   }
   
 
-"use strict";
-webix.ui({
-rows:[
-  { view:"template", 
-    type:"header", template:"My App!" },
-  { view:"datatable", 
-    autoConfig:true, 
-    data:{
-      title:"My Fair Lady", year:1964, votes:533848, rating:8.9, rank:5
-    }
-  }
-]
-});
+// "use strict";
+// webix.ui({
+// rows:[
+//   { view:"template", 
+//     type:"header", template:"My App!" },
+//   { view:"datatable", 
+//     autoConfig:true, 
+//     data:{
+//       title:"My Fair Lady", year:1964, votes:533848, rating:8.9, rank:5
+//     }
+//   }
+// ]
+// });
